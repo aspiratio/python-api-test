b67f02bd0bba535a283f763f329234b5fd3b5364
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-08-07 21:28:42 +0900

    アプリケーションの用意まで

---
+++ flask-tutorial/flaskr/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # アプリの作成と設定
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config is None:
+        # インスタンスフォルダにconfig.pyが存在する時だけロードする（見つからなくてもエラーを発生させない）
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # test_configが渡された時はそれをロードする
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)  # インスタンスフォルダがなければ作る
+    except OSError:
+        pass
+
+    # 確認用
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    return app

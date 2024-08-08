d3041e335dad82702f708a07bff107f495f61caf
YUHEI <80562226+aspiratio@users.noreply.github.com>
2024-08-08 11:10:04 +0900

    Blueprint と View まで終了

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -23,13 +23,12 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # 確認用
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
-
     from . import db
 
     db.init_app(app)
+
+    from . import auth
+
+    app.register_blueprint(auth.bp)
 
     return app
---
+++ flask-tutorial/flaskr/auth.py
@@ -0,0 +1,104 @@
+import functools
+
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "ユーザーネームは必須です"
+        elif not password:
+            error = "パスワードは必須です"
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:  # DBの整合性が取れていない時に出るエラー
+                error = f"ユーザー {username} は既に登録されています"
+            else:
+                return redirect(
+                    url_for("auth.login")
+                )  # Blueprintで”auth"という名前が付けられたviewのlogin関数にマッピングされたURLにリダイレクトするという意味
+
+        flash(error)
+
+    return render_template("auth/register.html")
+
+
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,).fetchone()
+        )
+
+        if user is None:
+            error = "存在しないユーザー名です"
+        elif not check_password_hash(user["password"], password):
+            error = "パスワードが違います"
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+
+@bp.before_app_request  # すべてのview関数の前に実行される関数
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
+
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+def login_required(view):  # viewをラップして新しいviewを返す
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
